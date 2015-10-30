858b42cf221685ad9438511c5380a1b7d5d77096
valentin-hc <valentin.hecqcauquil@gmail.com>
2015-10-30 17:22:04 +0100

    Add game of rooms JS refactored

--- Day5/gameofroomJS/game.js
+++ Day5/gameofroomJS/game.js
@@ -7,57 +7,61 @@ var Game = function(rooms){
 	// this.rooms = rooms;
 	this.current = 0;
 	this.player = new Player();
-}
+	this.options = {
+    prompt: (rooms[this.current].description + "\n>")
+	}
+};
 
+
+Game.prototype.roomAnswer = function (err, answer){
+	var command = this.translateAnswer(answer);
+	if (typeof command === "number") {
+		this.current = command;
+		this.options = {
+			prompt: (rooms[this.current].description + "\n>")
+		}
+		read(this.options, this.roomAnswer.bind(this));
+	}else {
+		console.log(command);
+		read(this.options, this.roomAnswer.bind(this));
+	}
+};
+
+Game.prototype.translateAnswer = function (answer) {
+	this.pickAndDrop(answer);
+	if (answer === "exit") {
+		process.exit();
+	}else if (answer === "N") {
+		return rooms[this.current].N;
+	}else if (answer === "E") {
+		return rooms[this.current].E;
+	}else if (answer === "S") {
+		return rooms[this.current].S;
+	}else if (answer === "W") {
+		return rooms[this.current].W;
+	}else if (answer === "Inventory"){
+		return this.player.inventory;
+	}else {
+		return "What was that?"
+	}
+};
+
+Game.prototype.pickAndDrop = function (answer) {
+	if (answer.substr(0,4) === "Pick") {
+		var object = answer.split("Pick up ")[1];
+		this.player.inventory.push(object);
+	} else if (answer.substr(0,4) === "Drop") {
+		var object = answer.split("Drop ")[1];
+		var index = this.player.inventory.indexOf(object);
+		if (index > -1) {
+			this.player.inventory.splice(index, 1);
+		}
+	}
+};
 
 
 Game.prototype.set = function() {
-	var player = this.player;
-	var inventory = player.inventory;
-	var current = this.current;
-	var options = {
-    prompt: (rooms[current].description + "\n>")
-	}
-	read(options, roomAnswer)
-
-	function roomAnswer (err, answer){
-		var command = translateAnswer(answer);
-		if (typeof command === "number") {
-			current = command;
-			options = {
-				prompt: (rooms[current].description + "\n>")
-			}
-			read(options, roomAnswer);
-		}else {
-			console.log(command);
-			read(options, roomAnswer);
-		}
-	}
-	
-	var translateAnswer = function (answer) {
-		if (answer === "exit") {
-			process.exit();
-		}else if (answer === "N") {
-			return rooms[current].N;
-		}else if (answer === "E") {
-			return rooms[current].E;
-		}else if (answer === "S") {
-			return rooms[current].S;
-		}else if (answer === "W") {
-			return rooms[current].W;
-		}else if (answer === "Inventory"){
-			return inventory;
-		}else {
-			return "What was that?"
-		}
-	}
-	var pickAndDrop = function (answer, items) {
-		if (answer.substr(0,4) === "Pick") {
-			var object = answer.split("Pick up ")[1];
-			items.push(object);
-		}
-	}
-
+	read(this.options, this.roomAnswer.bind(this));
 };
 
 
